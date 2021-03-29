8e60822e927a00f8a0b5b9a82eab26c82cd89cbd
Kolonskaia Anastasiia <anagrimonia@gmail.com>
2021-03-29 18:26:38 +0300

    Added tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,59 +1,42 @@
 const { expect } = require('chai');
 
-import input from './input.json';
-import output from './output.json';
+import input from './data/input/input.json';
+import inputDeepNested from './data/input/input-deep-nested.json';
+import inputSelfLike from './data/input/input-self-like.json';
 
-const { prepareData } = require('../src/index');
+import output from './data/output/output.json';
+import outputNotYet from './data/output/output-not-yet.json';
+import outputEmpty from './data/output/output-empty.json';
+import outputDeepNested from './data/output/output-deep-nested.json';
+import outputSelfLike from './data/output/output-self-like.json';
+
+
+const { prepareData } = require('../build/index');
+var fs = require('fs');
 
 describe('PrepareData tests', () => {
-    it('Checking an example', () => {
-        const data = prepareData(input, { sprintId: 974 })
-        
+    it('Checking result for an example', () => { 
+        const data = prepareData(input, { sprintId: 977 })
         expect(data).to.deep.equal(output);
     });
 
-    it('dummy test', () => { 
-
-         
+    it('Checking result for a not yet sprint', () => {
+        const data = prepareData(input, { sprintId: 996 })
+        expect(data).to.deep.equal(outputNotYet);
     });
 
-    it('dummy test', () => { 
-
-         
+    it('Checking result for an empty input array', () => { 
+      const data = prepareData([], { sprintId: 0 })
+      expect(data).to.deep.equal(outputEmpty);
     });
 
-    it('dummy test', () => { 
-
-         
+    it('Checking result for deep nested JSON', () => { 
+      const data = prepareData(inputDeepNested, { sprintId: 1 })
+      expect(data).to.deep.equal(outputDeepNested);
     });
 
-    it('dummy test', () => { 
-
-         
-    });
-
-    it('dummy test', () => { 
-
-         
-    });
-
-    it('dummy test', () => { 
-
-         
-    });
-
-    it('dummy test', () => { 
-
-         
-    });
-
-    it('dummy test', () => { 
-
-         
-    });
-
-    it('dummy test', () => { 
-
-         
+    it('Checking result for self-liking case', () => { 
+      const data = prepareData(inputSelfLike, { sprintId: 1 })
+      expect(data).to.deep.equal(outputSelfLike);
     });
 });
